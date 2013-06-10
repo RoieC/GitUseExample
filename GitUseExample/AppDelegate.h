72c414557aed2e8ce13e8b1d02e886df139b9cc7
Roie Cohen - Appstein <roiecohenappstein@Roie-Cohen-Appsteins-MacBook-Pro.local>
2013-06-10 14:22:34 +0300

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Roie Cohen - Appstein on 6/10/13.
+//  Copyright (c) 2013 Roie Cohen - Appstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitUseExample
+//
+//  Created by Roie Cohen - Appstein on 6/10/13.
+//  Copyright (c) 2013 Roie Cohen - Appstein. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
